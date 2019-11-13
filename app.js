5bff28906e299f2c41dcce05e5ea048e3998a8c1
bayleyjensen <bayley3jensen0@gmail.com>
2019-11-13 15:44:09 -0700

    works

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-function drawButtons() {
-  let template = "";
-  for (let i = 0; i < play.length; i++) {
-    const play[i] = play[i];
-    
+// function drawButtons() {
+//   let template = "";
+//   for (let i = 0; i < play.length; i++) {
+//     const play[i] = play[i];
+
+//   }
+// }
+// var computerchoice ["rock",
+//   "paper",
+//   "scissors"
+// ];
+
+// var randomchoice = computerchoice[Math.floor(Math.random()*computerchoice.length)];
+
+// document.body.innerHTML = randomchoice;
+
+function play(playersChoice) {
+  let computerChoice = "rock";
+  if (playersChoice == computerChoice) {
+    document.querySelector(`.bayley`).innerHTML = "Tie";
+  } else if (playersChoice == "paper") {
+    document.querySelector(`.bayley`).innerHTML = "You WIN";
+  } else if (playersChoice == "scissors") {
+    document.querySelector(`.bayley`).innerHTML = "You Lose!";
   }
 }
